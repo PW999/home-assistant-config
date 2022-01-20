080ce89f485126078079a478fa0006b2bc632636
Phillip <p_w999@ymail.com>
2022-01-20 08:11:29 +0100

    Move some of the generated sensors to v2

--- packages/v2/constants.rb
+++ packages/v2/constants.rb
@@ -1,6 +1,12 @@
-SENSOR_BEDROOM='sensor.bedroom'
-SENSOR_OUTSIDE='sensor.terrace'
-SENSOR_BATHROOM='sensor.bathroom'
-SENSOR_KITCHEN='sensor.bathroom'
-SENSOR_OFFICE='sensor.bureau'
-SENSOR_LIVING_ROOM='sensor.living_room'
+# frozen_string_literal: true
+
+SENSOR_BEDROOM = 'sensor.bedroom'
+SENSOR_OUTSIDE = 'sensor.terrace'
+SENSOR_BATHROOM = 'sensor.bathroom'
+SENSOR_KITCHEN = 'sensor.bathroom'
+SENSOR_OFFICE = 'sensor.bureau'
+SENSOR_LIVING_ROOM = 'sensor.living_room'
+
+MOTION_SENSOR_TOILET = 'sensor.toilet_presence'
+MOTION_SENSOR_PANTRY = 'sensor.pantry_presence'
+MOTION_SENSOR_HALL = 'sensor.hall_presence'
