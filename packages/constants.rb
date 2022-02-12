ce74c044edbe0ddaad1e67e23b819b452c89736d
Phillip <p_w999@ymail.com>
2022-02-12 12:57:21 +0100

    Small changes
    Fix: wrong sensor for kitchen
    Replaced motion sensor in pantry with a real door sensor

--- packages/constants.rb
+++ packages/constants.rb
@@ -3,10 +3,10 @@
 SENSOR_BEDROOM = 'sensor.bedroom'
 SENSOR_OUTSIDE = 'sensor.terrace'
 SENSOR_BATHROOM = 'sensor.bathroom'
-SENSOR_KITCHEN = 'sensor.bathroom'
+SENSOR_KITCHEN = 'sensor.kitchen'
 SENSOR_OFFICE = 'sensor.bureau'
 SENSOR_LIVING_ROOM = 'sensor.living_room'
 
 MOTION_SENSOR_TOILET = 'sensor.toilet_presence'
-MOTION_SENSOR_PANTRY = 'sensor.pantry_presence'
+MOTION_SENSOR_PANTRY = 'binary_sensor.pantry_door'
 MOTION_SENSOR_HALL = 'sensor.hall_presence'
